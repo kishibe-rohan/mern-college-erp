fd1a9b12098c7b26deeb953c9f2419ed82ba8dbf
kishibe-rohan <devrohan2000@gmail.com>
2022-01-23 17:32:19 +0530

    added reducers for admin faculty student

---
+++ client/src/redux/reducers/errorReducer.js
@@ -0,0 +1,14 @@
+import { SET_ERRORS } from "../actionTypes";
+
+const initialState = {};
+
+const setErrors = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_ERRORS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default setErrors;
